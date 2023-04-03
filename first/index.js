e33a7f77dfc602c1abe8f0aa8c83ea28fe44c644
Jan Deinhard <jan.deinhard@gmail.com>
2023-04-03 20:54:55 +0200

    simplify first example

--- first/index.js
+++ first/index.js
@@ -9,18 +9,12 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-function generatePrompt(commandLineTool) {
-  const capitalizedcommandLineTool =
-    commandLineTool[0].toUpperCase() + commandLineTool.slice(1).toLowerCase();
-  return `What are the basic commands for using the '${capitalizedcommandLineTool}' command line tool?"`;
-}
-
 (async () => {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: generatePrompt("less"),
+    prompt: "Say hello world!",
     temperature: 0.6,
-    max_tokens: 2048,
+    max_tokens: 100,
   });
 
   console.log(completion.data.choices[0].text);
